929517d60ce6ea4c0ea43bf4ae68a7f8adffd32a
bruno.araujo <bruno.araujo@accurate.com.br>
2020-05-22 19:04:50 -0300

    Updates

---
+++ precache-manifest.c41b17e1e02eb0dc07ad3f8ecbdb2e39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61eb860103fad742bed0c14d9629b7cb",
+    "url": "/snake_game/index.html"
+  },
+  {
+    "revision": "dfdf277bd32fa8f7a2ef",
+    "url": "/snake_game/static/css/main.591f3ae4.chunk.css"
+  },
+  {
+    "revision": "9adcf604474b3607094d",
+    "url": "/snake_game/static/js/2.a18797de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake_game/static/js/2.a18797de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfdf277bd32fa8f7a2ef",
+    "url": "/snake_game/static/js/main.e1621148.chunk.js"
+  },
+  {
+    "revision": "5aa73e629926ddce73ae",
+    "url": "/snake_game/static/js/runtime-main.0b9dcc33.js"
+  }
+]);
